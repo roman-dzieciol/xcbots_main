60fba6ea789898fd1be15e20c89f2f552049675b
Roman Dzięcioł <roman.dzieciol@gmail.com>
2014-08-12 15:21:05 +0200

    init

---
+++ xcbots_main/xcbots_main/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xcbots_main
+//
+//  Created by HighTail on 12/08/14.
+//  Copyright (c) 2014 HighTail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
